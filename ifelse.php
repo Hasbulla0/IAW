45587c4d58353fc31d833f41c3063d382326a86d
Hasbulla0 <abdellah.achiban.al@iespoligonosur.org>
2022-02-17 12:35:44 +0100

    Github IAW

---
+++ ifelse.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php
+
+		$x = 8 - 6.4;
+		$y = 1.6;
+
+		echo $x;
+		echo "<br>";
+		echo $y . "<br>";
+
+		var_dump($x);
+		echo "<br>";
+		var_dump($y);
+		if ($x == $y) {
+			echo "iguales";
+			} else{
+
+			echo "<br>";
+		echo "distintos";
+
+			}
+			echo "<br>";
+			var_dump(x == Y);
+
+		echo "<br>";
+		$x = round ($x,2);
+		$y = round ($y,2);
+
+		var_dump($x == $y);
+	?>
+
+
+</body>
+</html>
---
+++ round.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php
+
+		$x = 8 - 6.4;
+		$y = 1.6;
+
+		echo $x;
+		echo "<br>";
+		echo $y . "<br>";
+
+		var_dump($x);
+		echo "<br>";
+		var_dump($y);
+	?>
+
+
+</body>
+</html>
